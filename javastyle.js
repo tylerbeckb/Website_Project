c75ec1c1b99e6a38b95b388d167f320ce698cada
tylerbeckb <tylerbbrown@icloud.com>
2023-03-27 13:15:46 +0100

    feat:start button hides when clicked

--- javastyle.js
+++ javastyle.js
@@ -1,31 +1,8 @@
-// Executes code when window is loaded
-window.onload = function () {
-  // Contains questions and answers
-  var questionArray = {
-    "What is Reforestation?" : ["Act of destroying trees", "Act of replanting trees", "Protest for change"],
-    "What is not a way to help reduce deforestation?" : ["Watching TV", "Recycling", "Education"],
-  };
-  // Stores indexes of correct answers
-  var correctAnswer = [1,0];
-  var index = 0;
+const startBtn = document.getElementById("start")
 
-  // Function displays the questions
-  function displayQuestion(index) {
-    document.getElementById("questions").innerHTML = "";
-    var question = Object.keys(questionArray)[index];
-    document.getElementById("questions").innerHTML = question; 
-  };
+startBtn.addEventListener("click", startFunc)
 
-  // Function displays all answers
-  function displayAnswer(index) {
-    var answersArray = questionArray[Object.keys(questionArray)[index]];
-    document.getElementById("a1").innerHTML = "";
-    document.getElementById("a2").innerHTML = "";
-    document.getElementById("a3").innerHTML = "";
-    document.getElementById("a1").innerHTML = answersArray[0];
-    document.getElementById("a2").innerHTML = answersArray[1];
-    document.getElementById("a3").innerHTML = answersArray[2];
-  };
-  displayQuestion(index);
-  displayAnswer(index);
+function startFunc() {
+  startBtn.classList.add("hide")
 }
+
